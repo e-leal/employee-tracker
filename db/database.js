769850573d5a899825f392425e7d1dd6ae324c5b
e-leal <leal.elyssa03@gmail.com>
2020-08-09 23:36:57 -0500

    Add initial menu choice prompt to view all employees with correct data

--- db/database.js
+++ db/database.js
@@ -1,29 +1,13 @@
-const mysql2 = require('mysql2').verbose();
+const mysql2 = require('mysql2');
 
-//Connect to database
-const dbEmp = new mysql2.Database('./db/employee.db', err => {
-    if(err){
-        return console.error(err.message);
-    }
-    console.log('Connected to the employee database.');
-});
-
-const dbDept = new mysql2.Database('./db/department.db', err =>{
-    if(err){
-        return console.error(err.message);
-    }
-    console.log('Connected to the department database.');
-});
-
-const dbRole = new mysql2.Database('./db/role.db', err =>{
-    if(err){
-        return console.error(err.message);
-    }
-    console.log('Connected to the role database.');
-});
+// //Connect to database
+// const dbEmp = new mysql2.Database('./db/employee_recordDB.db', err => {
+//     if(err){
+//         return console.error(err.message);
+//     }
+//     console.log('Connected to the employee database.');
+// });
 
 
 
-module.exports = dbEmp;
-module.exports = dbDept;
-module.exports = dbRole;
+// module.exports = dbEmp;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql2');
 const db = require('./db/database');
+const questions = require('./lib/index')
 const express = require('express');
-const inquirer = require('require');
+const inquirer = require('inquirer');
 const fs = require('fs');
 const Employee = require('./lib/Employee');
 const PORT = process.env || 3001;
@@ -10,3 +11,60 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    // MySQL Username
+    user: 'root',
+    //mySQL password
+    password: 'Mar00nday03',
+    database: 'employee_recordDB'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connected as id '+ connection.threadId + '\n');
+});
+
+
+menuQuestions = () => {
+    return inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        default: 'View All Employees',
+        choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager']
+    }])
+    .then(choice => {
+        if(choice.action === 'View All Employees'){
+            viewAllEmps();
+        }
+    });
+};
+
+
+
+viewAllEmps = () => {
+    const query = connection.query(
+        "Select employee.first_name, employee.last_name, title, name, salary, CONCAT(emp.first_name, ' ', emp.last_name) Manager  from employee join role on employee.role_id = role.id join department on department.id = role.department_id left join employee emp on employee.manager_id = emp.id",
+        function(err, res){
+            if(err) throw err;
+            console.log(res);
+            connection.end();
+        });
+    
+    };
+
+menuQuestions();
+
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+  });
+// Start server after DB connection
+connection.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  });
